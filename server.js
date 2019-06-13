569ee555d12c89e8322b3ade64cd85fb3e450079
Elijah Logan <elijah.logan@purchase.edu>
2019-06-13 15:27:39 -0400

    connected to database, removed mongoose

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const mongodb = require('mongodb');
-const mongoose = require('mongoose');
+const url = 'mongodb://localhost:27017/todoList-db';
+const bodyParser = require('body-parser');
 
 
 
+// initailze express
 const app = express();
 
+//express middleware
+app.use(bodyParser.json());
+
+//have it respond to homepage request
 app.get('/', (req,res) => {
     res.send('hello world')
 });
 
-
-app.listen(3000, ()=> {
+//port its listening to responses on
+app.listen(5000, ()=> {
     console.log('server on')
+})
+
+const MongoClient = mongodb.MongoClient;
+
+MongoClient.connect(url, (err, db) => {
+    
+    console.log('Kudos. Connected successfully to server')
+    // Perform queries
+   
+    
 })
