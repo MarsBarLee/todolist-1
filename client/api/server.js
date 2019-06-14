67668be3d37185227f58019fa24347685ad7581c
Elijah Logan <elijah.logan@purchase.edu>
2019-06-14 17:48:01 -0400

    connected db to server

--- client/api/server.js
+++ client/api/server.js
@@ -4,8 +4,23 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = 4000;
+const mongoose = require('mongoose');
+const config = require('./DB.js');
 //what does cors do again?
 const cors = require('cors');
+
+//make mongoose into a promise
+//connect to db with url and resolve or check error->
+//whats happening with the .connect method
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+  () => {console.log('Database is connected') },
+  err => { console.log('Can not connect to the database'+ err)}
+);
+
+
+
+
 //middleware 
 app.use(cors());
 //urlencoded?
